d83dc769703912da726f6d18b94e6cb238c6de93
Mateusz Pitulski <jaskiermp@gmail.com>
2022-03-03 10:13:25 +0100

    Images completed

--- src/js/script.js
+++ src/js/script.js
@@ -97,10 +97,6 @@
     initAccordion() {
       const thisProduct = this;
 
-      /* find the clickable trigger (the element that should react to clicking) */
-      //const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-
-
       /* START: add event listener to clickable trigger on event click */
       thisProduct.accordionTrigger.addEventListener('click', function (event) {
         /* prevent default action for event */
@@ -118,7 +114,7 @@
         thisProduct.element.classList.toggle('active');
 
       });
-    
+
     }
 
     initOrderForm() {
@@ -143,7 +139,6 @@
 
     }
 
-
     processOrder() {
       const thisProduct = this;
       console.log('processOrder');
@@ -158,14 +153,12 @@
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
-
+      
         // for every option in this category
         for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
-
+       
           // check if there is param with a name of paramId in formData and if it includes optionId
           if (formData[paramId] && formData[paramId].includes(optionId)) {
             // check if the option is not default
@@ -180,15 +173,15 @@
               price = price - option.price;
             }
           }
-          let optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
-          console.log('optionImage:', optionImage);
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
 
-          if(optionImage = true && optionImage != null && formData[paramId] && formData[paramId].includes(optionId)) {
-            optionImage.classList.add(classNames.menuProduct.imageVisible);
-          } else {
-            optionImage.classList.remove(classNames.menuProduct.imageVisible);
+          if (optionImage) {
+            if (formData[paramId] && formData[paramId].includes(optionId)) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
           }
-         
         }
 
         // update calculated price in the HTML
@@ -201,8 +194,7 @@
     initMenu: function () {
       const thisApp = this;
       console.log('thisApp.data:', thisApp.data);
-      /*const testProduct = new Product();
-      console.log('testProduct:', testProduct);*/ // Zastąp zawartość metody app.initMenu następującym kodem:
+      
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
